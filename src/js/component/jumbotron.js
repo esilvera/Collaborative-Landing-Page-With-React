07b5d1c2e2256dcdc4a30243013dd6a8c1575700
Edwards Silvera <edwards.silvera@gmail.com>
2021-08-27 21:05:17 +0000

    Ejercicio Landing Page with React

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,23 @@
+// Jumbotron del Ejercicio Landing Page With React
+
+import React from "react";
+
+export function Jumbotron() {
+	return (
+		<div className="jumbotron">
+			<h1 className="display-4">A Warm Welcome!</h1>
+			<p className="lead">
+				Lorem Ipsum es simplemente el texto de relleno de las imprentas
+				y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+				estándar de las industrias desde el año 1500, cuando un impresor
+				(N. del T. persona que se dedica a la imprenta) desconocido usó
+				una galería de textos y los mezcló de tal manera que logró hacer
+				un libro de textos especimen
+			</p>
+
+			<a className="btn btn-primary btn-lg" href="#" role="button">
+				Learn more
+			</a>
+		</div>
+	);
+}
